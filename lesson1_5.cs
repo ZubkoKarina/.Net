aee5653cad43733c2c737469cff209567e58ab28
Karina Zubko <karinazubko2017@gmail.com>
2023-02-02 13:31:08 +0200

    lesson1

---
+++ lesson1_5.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+
+namespace DemoApplication
+{
+ class Program
+ { 
+  static void Main() 
+  {
+    int year;
+    Console.Write("Input an year : ");
+    year = Convert.ToInt32(Console.ReadLine());
+ 
+    if ((year % 400) == 0)
+ Console.WriteLine("366 days in {0} ", year);
+    else if ((year % 100) == 0)
+ Console.WriteLine("365 days in {0}", year);
+    else if ((year % 4) == 0)
+ Console.WriteLine("366 days in {0}", year);
+    else
+ Console.WriteLine("365 days in {0}", year);
+  }
+ }
+}
+
